607aa44d8921b04c353c0003416203bee347385e
0xAdachi <0xadachisakura@gmail.com>
2024-03-12 12:27:43 +0600

    dyn input F without jQ

--- JS/dynamicInputField/js/script.js
+++ JS/dynamicInputField/js/script.js
@@ -7,3 +7,31 @@ $("#addIn").on("click", () => {
 $(".input-field-wrapper").on("click",".remBtn", function() {
     $(this).parent().remove();
 })
+
+
+// ###### Without jQuery ###### //
+
+// 'use strict';
+
+// const addBtn = document.getElementById("addIn");
+// const inputFieldWrapper = document.querySelector(".input-field-wrapper");
+
+// addBtn.addEventListener("click", () => {
+//     const newInputField = document.createElement("div");
+//     const newInput = document.createElement("input");
+//     const newButton = document.createElement("button");
+//     newInput.type = "text";
+//     newInput.classList.add("extIn");
+//     newButton.textContent = "Remove";
+//     newButton.classList.add("remBtn");
+    
+//     newInputField.appendChild(newInput);
+//     newInputField.appendChild(newButton);
+
+//     inputFieldWrapper.appendChild(newInputField);
+
+
+//     newButton.addEventListener("click", () => {
+//         inputFieldWrapper.removeChild(newInputField);
+//     })
+// })
