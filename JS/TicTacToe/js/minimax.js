9751962ea40dcf8f570d77e20e71b854715770db
0xAdachi <0xadachisakura@gmail.com>
2024-03-29 17:32:49 +0600

    added depth counter to minimax

--- JS/TicTacToe/js/minimax.js
+++ JS/TicTacToe/js/minimax.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const HUMAN_PLAYER = 'O';
-const AI_PLAYER = 'X';
+// ### Game Constants ### //
+const HUMAN_PLAYER = "O";
+const AI_PLAYER = "X";
 let board = new Array(9).fill("");
 const winCombs = [
   [0, 1, 2],
@@ -17,64 +18,76 @@ let canPlay = true;
 const gameBoard = document.getElementById("game-board");
 const gameState = document.getElementById("gameState");
 
-gameBoard.addEventListener("click", ({target}) => {
-  if(canPlay) {
-  let currRow = target.parentNode.rowIndex;
-  let currCol = target.cellIndex;
-  if(gameBoard.rows[currRow]?.cells[currCol].innerHTML == "") {
-    let currIndex = (currRow * 3) + currCol;
-    board[currIndex] = HUMAN_PLAYER;
-    drawBoard(board, gameBoard);
-    
-    // ### AI Moves ### //
-    let bestAImove = minimax(board, AI_PLAYER).move;
-    board[bestAImove] = AI_PLAYER;
-    let score = checkBoardState(board); 
-    checkScore(score);
-    drawBoard(board, gameBoard);
+gameBoard.addEventListener("click", ({ target }) => {
+  // ### if the game is not over ### //
+  if (canPlay) {
+    let currRow = target.parentNode.rowIndex;
+    let currCol = target.cellIndex;
+    // ### if player chose a valid move ### //
+    if (gameBoard.rows[currRow]?.cells[currCol].innerHTML == "") {
+      let currIndex = currRow * 3 + currCol;
+      // ### take the move and update the board ### //
+      board[currIndex] = HUMAN_PLAYER;
+      drawBoard(board, gameBoard);
+
+      // ### AI figure outs the best move and takes it ### //
+      let bestAImove = minimax(board, 0, AI_PLAYER).move;
+      board[bestAImove] = AI_PLAYER;
+      let score = checkBoardState(board);
+      // ### we check if game is over or not ### //
+      checkScore(score);
+      drawBoard(board, gameBoard);
+    }
   }
-}});
+});
 
 function checkBoardState(board) {
-  for(let index in winCombs) {
+  // ### we check if current board has any winning combination ### //
+  for (let index in winCombs) {
     const [first, second, third] = winCombs[index];
-    if(board[first] !== "" && board[first] === board[second] && board[second] === board[third])
-    return board[first] === AI_PLAYER ? 10 : -10;
+    if (
+      board[first] !== "" &&
+      board[first] === board[second] &&
+      board[second] === board[third]
+    )
+      return board[first] === AI_PLAYER ? 10 : -10; // we return a score based on who won
   }
 
   const isBoardFull = board.every((square) => square !== "");
-  if(isBoardFull) return 0;
+  if (isBoardFull) return 0;  // return score of 0 if it's a tie
 
-  return 'P';
+  return "P";  // otherwise return that game is still in playing
 }
 
-function minimax(board, player) {
-  // ### First we check if the game is over, if yes we just return the winner or tie ### //
+function minimax(board, depth, player) {
+  // ### First we check if the game is over, if yes we just return the winner score or tie ### //
   const winner = checkBoardState(board);
-  if(winner !== 'P') return { score: winner, move: null };
+  if (winner !== "P") {
+    if(player === AI_PLAYER) return { score: winner - depth, move: null };  // taking depth into account so ai will chose shortest win
+    if(player === HUMAN_PLAYER) return { score: depth - winner, move: null };
+  }
 
+  // ### assume the best or worst possible future ### //
   let bestScore = player === AI_PLAYER ? -1000 : 1000;
   let bestMove = null;
 
-  for(let index = 0;  index < board.length; index++) {
-    if(board[index] === "") {
-      board[index] = player;
-      const { score, move } = minimax(board, player === AI_PLAYER ? HUMAN_PLAYER : AI_PLAYER);
+  // ### for the valid moves remaining ### //
+  for (let index = 0; index < board.length; index++) {
+    if (board[index] === "") {
+      board[index] = player;  // take the valid move and check all possible futures
+      const { score } = minimax(board, depth + 1, player === AI_PLAYER ? HUMAN_PLAYER : AI_PLAYER);
       board[index] = "";
-      if (player === AI_PLAYER && score > bestScore) {
-        bestScore = score;
+      if (player === AI_PLAYER && score > bestScore || player === HUMAN_PLAYER && score < bestScore) {
+        bestScore = score;  // update best score and best move accordingly with who's current player
         bestMove = index;
-      } else if (player === HUMAN_PLAYER && score < bestScore) {
-          bestScore = score;
-          bestMove = index;
-        }
+      }
     }
   }
 
-  return {score: bestScore, move: bestMove};
+  return { score: bestScore, move: bestMove };  // return the best score and best move
 }
 
-const convertBoardTo2d = (board, board2d) => board.map((currVal, index) => board2d[Math.floor(index / 3)][index % 3] = currVal);
+const convertBoardTo2d = (board, board2d) => board.map((currVal, index) => (board2d[Math.floor(index / 3)][index % 3] = currVal));
 // const convertBoardTo2d = (board, board2d) => {
 //   for (let i = 0; i < 3; i++) {
 //     for (let j = 0; j < 3; j++) {
@@ -83,23 +96,31 @@ const convertBoardTo2d = (board, board2d) => board.map((currVal, index) => board
 //   }
 // };
 
+// ### draw the board on screen ### //
 function drawBoard(board, gameBoard) {
-  let board2d = [["","",""],["","",""],["","",""]];
+  let board2d = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
   convertBoardTo2d(board, board2d);
-  for(let i = 0; i < board2d.length; i++) {
-    for(let j = 0; j < board2d[i].length; j++) {
-      gameBoard.rows[i].cells[j].innerHTML = board2d[i][j];
-    }
+  for (let i = 0; i < 9; i++) {
+    gameBoard.rows[Math.floor(i / 3)].cells[i % 3].innerHTML = board2d[Math.floor(i / 3)][i % 3];
   }
 }
 
+// ### checks the score to see game state and update display ### //
 function checkScore(score) {
-  if(score === 'P') {
+  if (score === "P") {
     gameState.textContent = "Playing Continues";
     gameState.style.color = "lime";
-  } else if(score == 10) {
+  } else if (score == 10) {
     gameState.textContent = "AI wins";
     gameState.style.color = "red";
+    canPlay = false;
+  } else if(score == -10) {
+    gameState.textContent = "HUMAN wins, which shouldn't happen. Please notice ABJ about this so he can fix the stupid code";
+    gameState.style.color = "gold";
     canPlay = false;
   } else {
     gameState.textContent = "It's a TIE";
